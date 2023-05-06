53f1c9fd00b2c80800febda63377c86b4fa56086
Julio Punkito <julesnoir@hotmail.fr>
2023-05-06 18:42:23 +0200

    Can merge data correctly, get them and update db with json files

--- server/src/routes/v1/scrapperdata.route.js
+++ server/src/routes/v1/scrapperdata.route.js
@@ -12,7 +12,11 @@ router.post('/ingredients', async (req, res) => {
       // Check if python id isn't already in db
       const alreadyExist = await Ingredient.findOne({ id: ingredient.id });
       if (!alreadyExist) {
-        const newIngredient = new Ingredient({ id: ingredient.id, name: ingredient.name, recipes: ingredient.recipes });
+        const newIngredient = new Ingredient({
+          id: ingredient.id,
+          name: ingredient.name,
+          recipes: ingredient.recipes,
+        });
         console.log('saving new ingredient :', ingredient.name);
         await newIngredient.save();
       }
@@ -26,7 +30,22 @@ router.post('/ingredients', async (req, res) => {
 
 router.post('/recipes', async (req, res) => {
   try {
-    await Recipe.insertMany(recipesjson);
+    for (const recipe of recipesjson) {
+      // Check if python id isn't already in db
+      const alreadyExist = await Recipe.findOne({ id: recipe.id });
+      if (!alreadyExist) {
+        const newRecipe = new Recipe({
+          id: recipe.id,
+          name: recipe.name,
+          recipes: recipe.recipes,
+          ingredients: recipe.ingredients,
+          otherIngredient: recipe.otherIngredient,
+          directions: recipe.directions,
+        });
+        console.log('saving new recipe :', recipe.name);
+        await newRecipe.save();
+      }
+    }
     res.status(201).send('Recettes insérés avec succès');
   } catch (error) {
     console.error(error);
@@ -34,4 +53,48 @@ router.post('/recipes', async (req, res) => {
   }
 });
 
+router.get('/recipes/:populate', async (req, res) => {
+  try {
+    const allRecipes = Boolean(req.params.populate) ? await Recipe.find().populate('ingredientsdb') : await Recipe.find();
+    return res.status(200).send(allRecipes);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Erreur lors de la récupération des recettes');
+  }
+});
+
+router.get('/ingredients/:populate', async (req, res) => {
+  try {
+    const allIngredients = Boolean(req.params.populate)
+      ? await Ingredient.find().populate('recipesdb')
+      : await Ingredient.find();
+
+    return res.status(200).send(allIngredients);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Erreur lors de la récupération des ingrédients');
+  }
+});
+
+router.post('/merge-data', async (req, res) => {
+  try {
+    const allingredients = await Ingredient.find();
+    for (const ingredient of allingredients) {
+      const recipes = await Recipe.find({ ingredients: { $elemMatch: { id: ingredient.id } } });
+
+      if (recipes && recipes.length > 0) {
+        ingredient.recipesdb = recipes;
+        await ingredient.save();
+        for (const recipe of recipes) {
+          recipe.ingredientsdb = ingredient;
+          await recipe.save();
+        }
+      }
+    }
+    return res.status(200).send('Data merged');
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send('Something wrong happend : ', error);
+  }
+});
 module.exports = router;
